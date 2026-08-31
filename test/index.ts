e8b9f630f1d2dc5d0679fdd24d4f39dc008979cd
Chris Bottin <cbottin@smartcommunications.com>
2026-08-31 20:26:21 +0100

    chore: Replace `glob` with `globSync` from `node:fs`

--- test/index.ts
+++ test/index.ts
@@ -1,13 +1,13 @@
 import xmlFormat, {XMLFormatterMinifyOptions, XMLFormatterOptions} from '../src/index';
 import {assert} from 'chai';
-import glob from 'glob';
+import {globSync} from 'node:fs';
 import path from 'path';
 import fs from 'fs';
 
 describe('XML formatter', function () {
 
-    function assertFormatError(src: string, formatterOptions: XMLFormatterOptions = {}) {
-        glob.sync(src).forEach(file => {
+    async function assertFormatError(src: string, formatterOptions: XMLFormatterOptions = {}) {
+        for (const file of globSync(src)) {
             it('Assert: ' + file, function() {
                 const fileContents = fs.readFileSync(file).toString('utf8');
                 const relativeFilePath = path.relative(process.cwd(), file);
@@ -26,11 +26,11 @@ describe('XML formatter', function () {
                     }
                 }
             });
-        });
+        }
     }
 
-    function assertFormat(src: string, formatterOptions: XMLFormatterOptions = {}) {
-        glob.sync(src).forEach(file => {
+    async function assertFormat(src: string, formatterOptions: XMLFormatterOptions = {}) {
+        for (const file of globSync(src)) {
             const outputFile = file.replace('-input', '-output');
 
             it('Assert: ' +  outputFile, function() {
@@ -48,11 +48,11 @@ describe('XML formatter', function () {
                 assert.equal(formattedContents, expectedContents, 'Formatted Content for ' + relativeFilePath);
                 assert.equal(formattedContents2, expectedContents, 'Idempotence test for ' + relativeFilePath);
             });
-        });
+        }
     }
 
-    function assertMinifyFormat(src: string, formatterOptions: XMLFormatterMinifyOptions = {}) {
-        glob.sync(src).forEach(file => {
+    async function assertMinifyFormat(src: string, formatterOptions: XMLFormatterMinifyOptions = {}) {
+        for (const file of globSync(src)) {
             const outputFile = file.replace('-input', '-output');
 
             it('Assert: ' +  outputFile, function() {
@@ -65,85 +65,85 @@ describe('XML formatter', function () {
                 assert.equal(formattedContents, expectedContents, 'Formatted Content for ' + relativeFilePath);
                 assert.equal(formattedContents2, expectedContents, 'Idempotence test for ' + relativeFilePath);
             });
-        });
+        }
     }
 
     context('should format XML with comments', function() {
-        assertFormat('test/data1/xml*-input.xml');
+        return assertFormat('test/data1/xml*-input.xml');
     });
 
     context('should format XML without comments', function() {
-        assertFormat('test/data2/xml*-input.xml', {filter: (node) => node.type !== 'Comment'});
+        return assertFormat('test/data2/xml*-input.xml', {filter: (node) => node.type !== 'Comment'});
     });
 
     context('should format XML without indenting text content when option is enabled:', function() {
-        assertFormat('test/data3/xml*-input.xml', {collapseContent: true});
+        return assertFormat('test/data3/xml*-input.xml', {collapseContent: true});
     });
 
     context('should format XML with various node types', function() {
-        assertFormat('test/data4/xml*-input.xml');
+        return assertFormat('test/data4/xml*-input.xml');
     });
 
     context('should format XML with the custom line separator', function() {
-        assertFormat('test/data5/xml*-input.xml', {lineSeparator: '\n'});
+        return assertFormat('test/data5/xml*-input.xml', {lineSeparator: '\n'});
     });
 
     context('should format XML that already contains line breaks', function() {
-        assertFormat('test/data6/xml*-input.xml');
+        return assertFormat('test/data6/xml*-input.xml');
     });
 
     context('should format XML adding a whitespace before self closing tag', function() {
-        assertFormat('test/data7/xml*-input.xml', {whiteSpaceAtEndOfSelfclosingTag: true});
+        return assertFormat('test/data7/xml*-input.xml', {whiteSpaceAtEndOfSelfclosingTag: true});
     });
 
     context('should escape a double quote in an attribute value', function() {
-        assertFormat('test/data8/xml*-input.xml');
+        return assertFormat('test/data8/xml*-input.xml');
     });
 
     context('should handle XML minification with collapseContent', function() {
-        assertMinifyFormat('test/data9/xml*-input.xml', {collapseContent: true});
+        return assertMinifyFormat('test/data9/xml*-input.xml', {collapseContent: true});
     });
 
     context('should handle XML minification without collapseContent (default)', function() {
-        assertMinifyFormat('test/data11/xml*-input.xml');
+        return assertMinifyFormat('test/data11/xml*-input.xml');
     });
 
     context('should fail when parsing invalid XML', function() {
-        assertFormatError('test/data10/xml*-input.xml');
+        return assertFormatError('test/data10/xml*-input.xml');
     });
 
     context('should fail silently when parsing invalid XML with throwOnFailure=false', function() {
-        assertFormatError('test/data10/xml*-input.xml', {throwOnFailure: false});
+        return assertFormatError('test/data10/xml*-input.xml', {throwOnFailure: false});
     });
 
     context('should format XML with spaces between tags when collapseContent=true', function() {
-        assertFormat('test/data12/xml*-input.xml', {collapseContent: true});
+        return assertFormat('test/data12/xml*-input.xml', {collapseContent: true});
     });
 
     context('should format XML with spaces between tags when collapseContent=false', function() {
-        assertFormat('test/data13/xml*-input.xml', {collapseContent: false});
+        return assertFormat('test/data13/xml*-input.xml', {collapseContent: false});
     });
 
     context('should ignore formatting on specified elements', function() {
-        assertFormat('test/data14/xml*-input.xml', {
+        return assertFormat('test/data14/xml*-input.xml', {
             collapseContent: true,
             ignoredPaths: ['/html/head/script', 'pre']
         });
     });
 
     context('should collapse empty tags when forceSelfClosingEmptyTag=true', function () {
-        assertFormat('test/data15/xml*-input.xml', { forceSelfClosingEmptyTag: true });
+        return assertFormat('test/data15/xml*-input.xml', { forceSelfClosingEmptyTag: true });
     });
     
     context('should not remove space with style before differently stylised word when prettifying xml', function () {
-        assertFormat('test/data16/xml*-input.xml', { collapseContent: true });
+        return assertFormat('test/data16/xml*-input.xml', { collapseContent: true });
     });
 
     context('use single quote attribute delimiter', function () {
-        assertFormat('test/data17/xml-single*-input.xml', { attributeQuotes: 'single' });
+        return assertFormat('test/data17/xml-single*-input.xml', { attributeQuotes: 'single' });
     });
 
     context('use double quote attribute delimiter', function () {
-        assertFormat('test/data17/xml-double*-input.xml', { attributeQuotes: 'double' });
+        return assertFormat('test/data17/xml-double*-input.xml', { attributeQuotes: 'double' });
     });
 });
